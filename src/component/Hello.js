77dfeb0769a07796f37ac4385efda6014ec89544
Park JiYoungs <piy0305@gmail.com>
2024-09-24 13:50:10 +0900

    컴포넌트별 css 관리하기

---
+++ src/component/Hello.js
@@ -0,0 +1,21 @@
+import World from './World';
+import styles from './Hello.module.css';
+
+export default function Hello() {
+    return (
+        <div>
+            <h1
+                style = {{
+                    color: "#f00",
+                    borderRight: "12px solid #000",
+                    marginBottom: "50px",
+                    opacity: 1,
+                }}
+            >
+                Hello
+            </h1>
+            <World/>
+            <div className={styles.box}>Hello</div>
+        </div> 
+    )
+};

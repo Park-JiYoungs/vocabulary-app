9304f0f452631abcdc33cb1c24491fc65442088c
Park JiYoungs <piy0305@gmail.com>
2024-09-25 21:16:14 +0900

    POST를 이용한 json파일 데이터 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 import EmptyPage from "./component/EmptyPage";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList/>} />
           <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word/" element={<CreateWord/>}/>
+          <Route path="/create_day/" element={<CreateDay/>}/>
           <Route path="*" element={<EmptyPage/>}/>
         </Routes>
       </div>

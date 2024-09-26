8a92dfa673c708ca6f2ca06cd0dbf102759980d4
Park JiYoungs <piy0305@gmail.com>
2024-09-26 17:14:26 +0900

    단어장 날짜 이동하기 (useState 활용 복습)

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,14 +1,33 @@
+import {useState} from "react";
 import useFetch from "../hooks/useFetch";
 import {useParams} from "react-router-dom";
 import Word from "./Word";
 
 export default function Day() {
-    const day = useParams().day;
+    const [day, setDay] = useState(useParams().day);
+    const days = useFetch("http://localhost:3001/days");
+
+    function toggleMove(move) {
+        let newDay = Number(day);
+
+        if (move === "prev") {
+            if (newDay === 1) alert("첫번쨰 페이지입니다.");
+            else setDay(newDay-1);
+        } else {
+            if (newDay === days.length) alert("마지막 페이지입니다.");
+            else setDay(newDay+1);
+        }
+    }
+
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return (
         <>
-            <h2>Day {day}</h2>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <button onClick={() => toggleMove("prev")}>PREV</button>
+                <h2>Day {day}</h2>
+                <button onClick={() => toggleMove("next")}>NEXT</button>
+            </div>
             {words.length === 0 && <span>Loading...</span>}
             <table>
                 <tbody>
